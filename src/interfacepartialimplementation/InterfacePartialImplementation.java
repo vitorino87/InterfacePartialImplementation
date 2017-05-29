7f646e1f46f0dc367a008934d563b7c54a668cb0
tiago.lucas <brother_87_22@hotmail.com>
2017-05-29 17:38:36 -0300

    versão 1

---
+++ src/interfacepartialimplementation/InterfacePartialImplementation.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacepartialimplementation;
+
+/**
+ *
+ * @author tiago.lucas
+ */
+public class InterfacePartialImplementation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        A a1  = new Test();
+        a1.showA();
+        a1.showB();
+    }
+    
+}
---
+++ src/interfacepartialimplementation/Test.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacepartialimplementation;
+
+/**
+ *
+ * @author tiago.lucas
+ */
+public class Test extends B{
+
+    @Override
+    public void showB() {
+        System.out.println("showB() of A interface");
+    }
+    
+}
